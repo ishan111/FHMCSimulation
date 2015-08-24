aa6881bfc77418365ff88b15c6e343a081eab988
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2015-08-23 23:31:02 -0400

    Added mutidimensional histogram

---
+++ src/histogram.cpp
@@ -0,0 +1,117 @@
+#include "histogram.h"
+
+/*!
+ * Instantiate a multidimensional histogram.  Bounds and widths must be specified for each dimension.
+ * A bin is considered "centered" on its value.
+ *
+ * \param [in] lbound Vector of lower bounds for each dimension
+ * \param [in] ubound Vector of upper bounds for each dimension
+ * \param [in] nbins Number of bins to use along each dimension
+ */
+histogram::histogram (const std::vector <double> lbound, const std::vector <double> ubound, const std::vector <long long unsigned int> nbins) {
+    if (lbound.size() != ubound.size()) {
+        throw customException ("Upper and lower bounds for histogram do have the same size");
+    }
+    if (nbins.size() != lbound.size()) {
+        throw customException ("Number of bins for histogram's dimensions does not have the same size as its bounds");
+    }
+    
+    dim_ = nbins.size();
+    widths_.resize(dim_, 0);
+    delta_.resize(dim_, 0);
+    
+    size_ = 1;
+    for (unsigned int i = 0; i < dim_; ++i) {
+        if (lbound[i] >= ubound[i]) {
+            throw customException ("Lower bound >= upper bound illegal for a histogram");
+        }
+        if (nbins[i] <= 1) {
+            throw customException ("Must > 1 bins for each dimensions in the histogram");
+        }
+        size_ *= nbins[i];
+        delta_[i] = (ubound_[i] - lbound_[i])/(nbins_[i]-1);
+        
+        // build projected widths
+        if (i == 0) {
+            widths_[i] = 1;
+        } else {
+            widths_[i] = widths_[i-1]*nbins[i-1];
+        }
+    }
+    
+    lbound_ = lbound;
+    ubound_ = ubound;
+    nbins_ = nbins;
+    
+    // initialize the histogram to 0
+    try {
+        h_.resize(size_, 0);
+    } catch (std::bad_alloc &ba) {
+        throw customException ("Out of memory for histogram");
+    }
+}
+
+/*!
+ * Increment the histogram by a given value at a given address.
+ *
+ * \param [in] address Address of the histogram to increment
+ * \param [in] val Value to add to the histogram at address
+ */
+void histogram::increment (const long long unsigned int address, const double val) {
+    if (address < size_) {
+        h_[address] += val;
+    } else {
+        throw customException ("Histogram address out of bounds");
+    }
+}
+
+/*!
+ * Increment the histogram by a given value at a given coordinate.
+ *
+ * \param [in] coords Vector of coordinates correponding to a location in the histogram to increment
+ * \param [in] val Value to add to the histogram at address
+ */
+void histogram::increment (const std::vector <double> &coords, const double val) {
+    long long unsigned int address = 0;
+    try {
+        address = getAddress (coords);
+    } catch (customException &ce) {
+        throw customException ("Histogram address out of bounds");
+    }
+    h_[address] += val;
+}
+
+/*!
+ * Get the linear address of the multidimensional coordinate.
+ *
+ * \param [in] coords Coordinates
+ */
+const long long unsigned int histogram::getAddress (const std::vector <double> &coords) {
+    if (coords.size() != dim_) {
+        throw customException ("Illegal number of coordinate dimensions, cannot locate histogram address");
+    }
+    long long unsigned int address = 0;
+    for (unsigned int i = 0; i < dim_; ++i) {
+        address += round((coords[i] - lbound_[i])/delta_[i])*widths_[i]; // will work safely for integers too
+    }
+    return address;
+}
+
+/*!
+ * Given an address, return the (center of the) coordinate this refers to.
+ *
+ * \param [in] address Address to check
+ */
+const std::vector <double> histogram::getCoords (long long unsigned int address) {
+    std::vector <double> coords (dim_, 0);
+    if (address >= size_) {
+        throw customException ("Histogram address out of bounds");
+    }
+    for (unsigned int i = size_-1; i > 0; --i) {
+        long long int diff = floor(address/widths_[i]);
+        coords[i] = diff;
+        address -= diff*widths_[i];
+    }
+    coords[0] = address;
+    return coords;
+}
---
+++ src/histogram.h
@@ -0,0 +1,30 @@
+#ifndef HISTOGRAM_H_
+#define HISTOGRAM_H_
+
+#include <vector>
+#include <cmath>
+#include "global.h"
+
+class histogram {
+public:
+    ~histogram () {;}
+    histogram (const std::vector <double> lbound, const std::vector <double> ubound, const std::vector <long long unsigned int> nbins);
+    
+    void increment (const long long unsigned int address, const double val);
+    void increment (const std::vector <double> &coords, const double val);
+    const long long unsigned int getAddress (const std::vector <double> &coords);
+    const std::vector <double> getCoords (long long unsigned int address);
+    std::vector <double> getHistogram () { return h_; } //!< Return the current histogram
+    
+private:
+    int dim_; //!< Dimensionality of the histogram
+    long long unsigned int size_; //!< Total size of histogram
+    std::vector <long long unsigned int> nbins_; //!< Number of bins along each dimension
+    std::vector <long long unsigned int> widths_; //!< Total widths (number of bins) of each iteratively projected dimension
+    std::vector <double> h_; //!< Histogram
+    std::vector <double> ubound_; //!< Upper bound for each dimension
+    std::vector <double> lbound_; //!< Lower bound for each dimension
+    std::vector <double> delta_; //!< Magnitude of the width of a bin in each dimension
+};
+
+#endif
