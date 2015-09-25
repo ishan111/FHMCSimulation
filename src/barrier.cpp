455f4e86dd01b0e408b16a39a7831f993f64368e
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2015-09-24 20:03:19 -0400

    Implemented walls

--- src/barrier.cpp
+++ src/barrier.cpp
@@ -1,16 +1,21 @@
-#include "barrier"
+#include "barrier.h"
 
 /*!
  * Instantiate a hard wall with boundaries in the +/- z direction.
  *
  * \param [in] lb z-Position of the lower wall
  * \param [in] ub z-Position of the upper wall
+ * \param [in] sigma Hard-sphere diameter the species this wall interacts with can approach within
  */
-hardWallZ::hardWallZ (const double lb, const double ub) {
+hardWallZ::hardWallZ (const double lb, const double ub, const double sigma) {
     if (lb >= ub) {
         throw customException ("hardWallZ must have lower bound < upper bound");
     }
+    if (sigma < 0) {
+        throw customException ("hardWallZ must have sigma >= 0");
+    }
     
+    sigma_ = sigma;
     ub_ = ub;
     lb_ = lb;
 }
@@ -19,15 +24,91 @@ hardWallZ::hardWallZ (const double lb, const double ub) {
  * Return whether or not a point falls between the walls (subject to a hard-sphere exclusion radius).
  *
  * \param [in] point (x, y, z) Point to test - this does NOT need to be in the simulation box a priori
- * \param [in] params Vector containing one entry: the hard-sphere DIAMETER of the particle (how close to allow the sphere to approach the wall's surface)
  * \param [in] box Simulation box
  */
-bool hardWallZ::inside (const std::vector < double > &point, const std::vector < double > params, const std::vector < double > &box) {
+bool hardWallZ::inside (const std::vector < double > &point, const std::vector < double > &box) {
     std::vector < double > p = point;
     pbc (p, box);
-    if (p.z => ub_ - params[0]/2.0 || pz <= lb_ + params[0]/2.0) {
+    if (p[2] >= ub_ - sigma_/2.0 || p[2] <= lb_ + sigma_/2.0) {
         return false;
     } else {
         return true;
     }
+}
+
+/*!
+ * Instantiate a square well wall with boundaries in the +/- z direction.
+ *
+ * \param [in] lb z-Position of the lower wall
+ * \param [in] ub z-Position of the upper wall
+ * \param [in] sigma Hard-sphere diameter the species this wall interacts with can approach within
+ * \param [in] range Distance normal to the wall's surface where there is an interaction
+ * \param [in] eps Magnitude of the wall interaction (U = -eps)
+ */
+squareWellWallZ::squareWellWallZ (const double lb, const double ub, const double sigma, const double range, const double eps) {
+    if (lb >= ub) {
+        throw customException ("squareWellWallZ must have lower bound < upper bound");
+    }
+    if (sigma < 0) {
+        throw customException ("squareWellWallZ must have sigma >= 0");
+    }
+    if (range < 0) {
+        throw customException ("squareWellWallZ must have range >= 0");
+    }
+    if (eps < 0) {
+        throw customException ("squareWellWallZ must have eps >= 0");
+    }
+    if (sigma/2.0 >= range) {
+        throw customException ("squareWellWallZ must have sigma/2 < range to have a finite range of interaction");
+    }
+    
+    sigma_ = sigma;
+    range_ = range;
+    ub_ = ub;
+    lb_ = lb;
+}
+
+/*!
+ * Return whether or not a point falls between the walls (subject to a hard-sphere exclusion radius).
+ *
+ * \param [in] point (x, y, z) Point to test - this does NOT need to be in the simulation box a priori
+ * \param [in] box Simulation box
+ */
+bool squareWellWallZ::inside (const std::vector < double > &point, const std::vector < double > &box) {
+    std::vector < double > p = point;
+    pbc (p, box);
+    if (p[2] >= ub_ - sigma_/2.0 || p[2] <= lb_ + sigma_/2.0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/*!
+ * Interaction energy with the wall.
+ *
+ * \param [in] point (x, y, z) Point to test - this does NOT need to be in the simulation box a priori
+ * \param [in] box Simulation box
+ */
+double squareWellWallZ::energy (const std::vector < double > &point, const std::vector < double > &box) {
+    std::vector < double > p = point;
+    pbc (p, box);
+    double U = 0.0;
+    
+    // return infinity if out of bounds
+    if (p[2] >= ub_ - sigma_/2.0 || p[2] <= lb_ + sigma_/2.0) {
+        return NUM_INFINITY;
+    }
+    
+    // interaction with top wall
+    if (p[2] > ub_ - range_) {
+        U += -eps_;
+    }
+    
+    // interaction with lower wall
+    if (p[2] < lb_ + range_) {
+        U += -eps_;
+    }
+    
+    return U;
 }
--- src/barrier.h
+++ src/barrier.h
@@ -4,15 +4,14 @@
 #include <vector>
 #include "global.h"
 #include "utilities.h"
+#include "potentials.h"
 
 /*!
- * Virtual base class for barriers.
+ * Virtual base class for barriers.  It is intended that there should be a separate barrier class defined for each species type.
  */
 class barrier {
 public:
-	barrier () {};
-	virtual ~barrier () = 0;
-    bool inside (const std::vector < double > &point, const std::vector < double > params, const std::vector < double > &box) = 0;
+    virtual bool inside (const std::vector < double > &point, const std::vector < double > &box) = 0;
 };
 
 /*!
@@ -20,12 +19,32 @@ public:
  */
 class hardWallZ : public barrier {
 public:
-    ~hardWallZ () {};
-    hardWallZ (const double lb, const double ub);
-    bool inside (const std::vector < double > &point, const std::vector < double > params, const std::vector < double > &box);
+    ~hardWallZ ();
+    hardWallZ (const double lb, const double ub, const double sigma);
+    bool inside (const std::vector < double > &point, const std::vector < double > &box);
     
 private:
     double lb_; //!< Lower bound for wall in the z-direction
     double ub_; //!< Uppber bound for all in the z-direction
+    double sigma_; //!< Hard-sphere diameter the species this wall interacts with can approach within
 };
+
+/*!
+ * Parallel square-well walls in the z-direction.
+ */
+class squareWellWallZ : public barrier {
+public:
+    ~squareWellWallZ () {};
+    squareWellWallZ (const double lb, const double ub, const double sigma, const double range, const double eps);
+    bool inside (const std::vector < double > &point, const std::vector < double > &box);
+    double energy (const std::vector < double > &point, const std::vector < double > &box);
+    
+private:
+    double lb_; //!< Lower bound for wall in the z-direction
+    double ub_; //!< Uppber bound for all in the z-direction
+    double range_; //!< Distance normal to the wall's surface where there is an interaction
+    double eps_; //!< Magnitude of the interaction
+    double sigma_; //!< Hard-sphere diameter the species this wall interacts with can approach within
+};
+
 #endif
