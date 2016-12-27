c34dc2509947e1e6e51ac27258e3c13ba776393f
mahynski <nathan.mahynski@gmail.com>
2016-12-27 10:45:12 -0500

    updated raritan submission

--- helper/window_helper.py
+++ helper/window_helper.py
@@ -33,8 +33,8 @@ def pure_settings (settings):
 	info["box"] = [8.0, 8.0, 8.0]
 	info["mu"] = [0.0*i for i in xrange(info["num_species"])]
 	info["seed"] = -10
-	info["max_N"] = int(1200)
-	info["min_N"] = int(0)
+	info["max_N"] = [int(1200)]s
+	info["min_N"] = [int(0)]
 	info["window"] = [bounds[0], bounds[1]]
 	info["restart_file"] = ""
 	info["num_expanded_states"] = int(1)
@@ -93,32 +93,33 @@ def make_sleeper (filename):
 	f.write('for ((i = 0; i < 100000; ++i)); do\n\tfor ((j = 0; j < 100000; ++j)); do\n\t\tsleep $((1*'+str(int(sync_every))+'*60));\n\t\trsync -a $1 $2;\n\tdone;\ndone;')
 	f.close()
 
-def gibbs_qsub (prefix, num_windows, binary, git_head, input_name="input.json", jobs_per_node=12, scratch_dir="/tmp/nam4/", q="medium", tag=""):
+def gibbs_qsub (num_windows, binary, git_head, tag, prefix, input_name="input.json", jobs_per_node=12, q="medium", scratch_dir="/tmp/nam4/"):
 	"""
 	Example of submission script for PBS system, in this case, for gibbs.nist.gov.
 	This produces qsub_X.pbs files, for as many X are necessary.
 
 	Parameters
 	----------
-	prefix : str
-		Directory to place qsub files
 	num_windows : int
 		Number of total windows
 	binary : str
 		Path to binary (FHMCSimulation) to execute
 	git_head : str
 		Path to git head log
+	tag : str
+		Name of this job
+	prefix : str
+		Directory to place qsub files
 	input_name : str
 		Name of local input file inside window directory (default=input.json)
 	jobs_per_node : int
 		Number of jobs per node to allow (default=12)
+	q : str
+		Name of the queue to submit to (default="medium")
 	scratch_dir : str
 		Absolute path to scratch space for user
-	q : str
-		Name of the queue to submit to
-	tag : str
-		Name of this job
-
+	
+	
 	"""
 
 	base_string = "#!/bin/bash\n#PBS -l nodes=1:ppn=__PPNVIS__\n#PBS -q __QUEUE__\n#PBS -V\n#PBS -N __TAGNAME__\n#PBS -M nathan.mahynski@nist.gov\n#PBS -m a\n\n# move to dir pbs was launched from\ncd $PBS_O_WORKDIR;\n\n# report\necho \"Running on $(hostname)\";\necho \"Time: $(date)\";\necho \"Starting directory: $PWD\";\nheaddir=$PWD;\npids=\"\";\nfor i in {__MINWIN__..__MAXWIN__}; do\n\t# create and move to tmpdir\n\ttmpdir=__SCRATCHDIR__/$PBS_JOBID/$i;\n\thomedir=$headdir/$i;\n\tmkdir -p $tmpdir;\n\techo \"Moving to temporary directory: $tmpdir\";\n\tcp -r $homedir/* $tmpdir/;\n\tcd $tmpdir;\n\n\t# get info about binary\n\ttail -1 __GITHEAD__ > binary.info;\n\n\t# run sleeper and binary\n\tsh sleeper.sh ./ $homedir &\n\t__BINARY__ __INPUTNAME__ 2>> err >> log &\n\tpids=\"$pids $!\"\ndone\n\n# wait for all process ids\nfor p in $pids; do\n\twait $p;\ndone\n\n# final sync and clean up\ncd $headdir;\nsids=\"\";\nfor i in {__MINWIN__..__MAXWIN__}; do\n\ttmpdir=__SCRATCHDIR__/$PBS_JOBID/$i;\n\thomedir=$headdir/$i;\n\techo \"Final sync from $tmpdir to $homedir\";\n\trsync -a $tmpdir/ $homedir/;\n\techo \"Removing $tmpdir\";\n\trm -r $tmpdir;\n\tsids=\"$sids $!\"\ndone\n\n# wait for all syncs to finish\nfor s in $sids; do\n\twait $s;\ndone\n\necho \"Finished on $(date)\";"
@@ -128,6 +129,72 @@ def gibbs_qsub (prefix, num_windows, binary, git_head, input_name="input.json", 
 	new_string = re.sub('__GITHEAD__', str(git_head), new_string)
 	new_string = re.sub('__BINARY__', str(binary), new_string)
 	new_string = re.sub('__INPUTNAME__', str(input_name), new_string)
+
+	jobs_remaining = num_windows
+	for idx in xrange(0, int(m.ceil(num_windows/float(jobs_per)))):
+		start = idx*jobs_per+1
+		end = idx*jobs_per+min([jobs_remaining,jobs_per])
+		i_string = re.sub('__MINWIN__', str(start), new_string)
+		i_string = re.sub('__MAXWIN__', str(end), i_string)
+		i_string = re.sub('__TAGNAME__', str(tag+"_"+str(idx+1)), i_string)
+		i_string = re.sub('__PPNVIS__', str(2*(end-start+1)), i_string) # double to deal with hyperthreading
+		jobs_remaining -= jobs_per
+
+		f = open(prefix+"/qsub_"+str(idx+1)+".pbs", 'w')
+		f.write(i_string)
+		f.close()
+
+	return
+
+def raritan_sbatch (num_windows, binary, git_head, tag, prefix, input_name="input.json", jobs_per_node=12, q="mml", hours=72, scratch_dir="/scratch/nam4/"):
+	"""
+	Example of submission script for PBS system, in this case, for raritan.nist.gov.
+	This produces sbatch_X.sb files, for as many X are necessary.
+
+	Parameters
+	----------
+	num_windows : int
+		Number of total windows
+	binary : str
+		Path to binary (FHMCSimulation) to execute
+	git_head : str
+		Path to git head log
+	tag : str
+		Name of this job
+	prefix : str
+		Directory to place sbatch files	
+	input_name : str
+		Name of local input file inside window directory (default=input.json)
+	jobs_per_node : int
+		Number of jobs per node to allow (default=12)
+	q : str
+		Name of the queue to submit to (default="mml")
+	hours : int
+		Number of hours to submit the job for (default=72)
+	scratch_dir : str
+		Absolute path to scratch space for user (default="/scratch/nam4/")
+
+	"""
+
+	totmem = 100*num_windows # MB/node, assuming 100MB per job
+	sdays = int(floor(float(hours)/24.))
+	shours = int(floor(float(hours-24*sdays)))
+	sminutes = int(floor(float(hours-24*sdays-shours)*60))
+
+	if (sdays > 14 or (sdays == 14 and (shours > 0 or sminutes > 0))):
+		raise Exception ("Cannot exceed 14 days on raritan queues")
+
+	base_string = "#!/bin/bash\n#\n#SBATCH -n __PPNVIS__\n#SBATCH -N 1\n#SBATCH -p __QUEUE__\n#SBATCH -t __SDAYS__:__SHOURS__:__SMINUTES__\n#SBATCH -m __TOTMEM__\n#SBATCH -o hostname_%j.out\n#SBATCH -e hostname_%j.err\n#SBATCH --mail-type=FAIL\n#SBATCH --mail-use nathan.mahynski@nist.gov\n#SBATCH -J __TAGNAME__\n#SBATCH --export=all\n\n# move to dir pbs was launched from\ncd $SLURM_SUBMIT_DIR;\n\n# report\necho \"Running on $(hostname)\";\necho \"Time: $(date)\";\necho \"Starting directory: $PWD\";\nheaddir=$PWD;\npids=\"\";\nfor i in {__MINWIN__..__MAXWIN__}; do\n\t# create and move to tmpdir\n\ttmpdir=__SCRATCHDIR__/$SLURM_JOBID/$i;\n\thomedir=$headdir/$i;\n\tmkdir -p $tmpdir;\n\techo \"Moving to temporary directory: $tmpdir\";\n\tcp -r $homedir/* $tmpdir/;\n\tcd $tmpdir;\n\n\t# get info about binary\n\ttail -1 __GITHEAD__ > binary.info;\n\n\t# run sleeper and binary\n\tsh sleeper.sh ./ $homedir &\n\t__BINARY__ __INPUTNAME__ 2>> err >> log &\n\tpids=\"$pids $!\"\ndone\n\n# wait for all process ids\nfor p in $pids; do\n\twait $p;\ndone\n\n# final sync and clean up\ncd $headdir;\nsids=\"\";\nfor i in {__MINWIN__..__MAXWIN__}; do\n\ttmpdir=__SCRATCHDIR__/$SLURM_JOBID/$i;\n\thomedir=$headdir/$i;\n\techo \"Final sync from $tmpdir to $homedir\";\n\trsync -a $tmpdir/ $homedir/;\n\techo \"Removing $tmpdir\";\n\trm -r $tmpdir;\n\tsids=\"$sids $!\"\ndone\n\n# wait for all syncs to finish\nfor s in $sids; do\n\twait $s;\ndone\n\necho \"Finished on $(date)\";"
+
+	new_string = re.sub('__QUEUE__', str(q), base_string)
+	new_string = re.sub('__SCRATCHDIR__', str(scratch_dir), new_string)
+	new_string = re.sub('__GITHEAD__', str(git_head), new_string)
+	new_string = re.sub('__BINARY__', str(binary), new_string)
+	new_string = re.sub('__INPUTNAME__', str(input_name), new_string)
+	new_string = re.sub('__TOTMEM__', str(totmem), new_string)
+	new_string = re.sub('__SDAYS__', str(sdays), new_string)
+	new_string = re.sub('__SHOURS__', str(shours), new_string)
+	new_string = re.sub('__SMINUTES__', str(sminutes), new_string)
 
 	jobs_remaining = num_windows
 	for idx in xrange(0, int(m.ceil(num_windows/float(jobs_per)))):
@@ -195,6 +262,6 @@ if __name__ == "__main__":
 		make_input (dname+"/"+input_name, (bounds[w], beta))
 		make_sleeper (dname+"/sleeper.sh")
 
-	gibbs_qsub ('./', num_windows, binary, git_head, input_name, jobs_per, scratch_dir, q, tag)
+	gibbs_qsub (num_windows, binary, git_head, tag, './', input_name, jobs_per, q, scratch_dir)
 	"""
 
