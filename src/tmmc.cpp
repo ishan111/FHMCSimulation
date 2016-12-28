ce5ef5ea7bf8b8b1e9babe54eaae374a348e1b54
mahynski <nathan.mahynski@gmail.com>
2016-12-28 11:20:56 -0500

    sync before working on one_dim_hist

--- src/tmmc.cpp
+++ src/tmmc.cpp
@@ -45,6 +45,7 @@ void performTMMC (simSystem &sys, checkpoint &res, moves *usedMovesPr) {
     } else {
         printCounter = res.moveCounter;
         sweep = res.sweepCounter;
+        sys.reInitializeEnergyHistogram();
     }
 
     std::cout << "Starting progress stage from " << printCounter << "/" << std::min(numSweepSnaps, sys.totalTMMCSweeps) << " at " << getTimeStamp() << std::endl;
