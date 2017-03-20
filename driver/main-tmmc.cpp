2653d88d73a2d5b603edaa504b3e8dc5f97cd622
mahynski <nathan.mahynski@gmail.com>
2017-03-20 14:45:29 -0400

    updated checkpoints to 10 min

--- driver/main-tmmc.cpp
+++ driver/main-tmmc.cpp
@@ -10,7 +10,7 @@ int main (int argc, char * const argv[]) {
 	simSystem sys = initialize (argv[1], &usedMovesEq, &usedMovesPr);
 
 	// If checkpoint exists, default to use this information to restart the simulation
-	checkpoint cpt ("checkpt", 15, sys); //900
+	checkpoint cpt ("checkpt", 600, sys);
     if (!cpt.hasCheckpoint) {
         setConfig (sys, argv[1]);
     }
